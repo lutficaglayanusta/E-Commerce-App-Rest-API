b34689bd7cf6bf69d5855e9e7d7b2fb2e7a4f6e6
caglayan <caglayanustaa@gmail.com>
2025-02-26 02:17:22 +0300

    e commerce codecademy

--- controller/authController.js
+++ controller/authController.js
@@ -1,21 +1,34 @@
 import pool from "../helpers/database/connectDatabase.js";
+import asyncErrorWrapper from "express-async-handler";
+import bcrypt from "bcrypt"
+import CustomError from "../helpers/error/CustomError.js";
 
-export const userRegister = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
+export const userRegister = asyncErrorWrapper(async (req, res,next) => {
+  const { name, email, password } = req.body;
 
-        let users = await pool.query("INSERT INTO users (name,email,password) VALUES($1,$2,$3) RETURNING * ",[name,email,password])
+  const checkResult = await pool.query("SELECT * FROM users WHERE email =$1", [
+    email,
+  ]);
 
-        users = users.rows[0]
+  if (checkResult.rows.length > 0) {
+    return next(new CustomError("User already exit", 400));
+  } else {
+    bcrypt.hash(password, 10, async (err, hash) => {
+      if (err) {
+        next(err);
+      } else {
+        let users = await pool.query(
+          "INSERT INTO users (name,email,password) VALUES($1,$2,$3) RETURNING * ",
+          [name, email, hash]
+        );
+
+        users = users.rows[0];
 
         res.status(200).json({
-            success: "true",
-            users
-        })
-    } catch (err) {
-        res.status(400).json({
-            success: "fail",
-            err
-        })
-    }
-}
+          success: "true",
+          users,
+        });
+      }
+    });
+  }
+});
---
+++ middleware/customErrorHandler.js
@@ -0,0 +1,19 @@
+import CustomError from "../helpers/error/CustomError.js";
+
+const customErrorHandler = (err, req, res, next) => {
+    let customError = err;
+    
+    if (err.name === "SyntaxError") {
+      customError = new CustomError("Unexpected Syntax", 400);
+    }
+    if (err.name === "ValidationError") {
+      customError = new CustomError(err.message, 400);
+    }
+
+  res.status(customError.status || 500).json({
+    success: false,
+    message: customError.message || "Internal Server Error",
+  });
+  
+};
+export default customErrorHandler
